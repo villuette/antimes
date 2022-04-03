53907b679d6c6f5d606bdf6c29ae93bce54f5f4d
Denis <muharamov14@gmail.com>
2022-04-04 00:12:58 +0500

    Добавил хп над мобами

--- Assets/Scripts/Fights.cs
+++ Assets/Scripts/Fights.cs
@@ -1,25 +1,24 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Fights : MonoBehaviour
 {
     private bool attack = false;
-    private int GG_health = Stats.GG_Health;
-    private int GG_damage = Stats.GG_Damage;
-    private float GG_armor = Stats.GG_Armor;
     private int Enemy_Health;
     private int Enemy_Damage;
-    
+    public float coord_x, coord_y;
+    [SerializeField] private Text enemy_hp;
+
     void Start()
     {
         Stats_Enemy enemy = gameObject.AddComponent<Stats_Enemy>();
         Enemy_Health = enemy.Enemy_Health;
         Enemy_Damage = enemy.Enemy_Damage;
     }
-
-    void Update()
+    private void Update()
     {
-        
+        ShowEnemyHp();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -34,19 +33,33 @@ public class Fights : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             attack = false;
+            enemy_hp.text = null;
             StopCoroutine(FightCoroutine());
+        }
+    }
+
+    // Выводит над мобами их хп
+    private void ShowEnemyHp()
+    {
+        if (attack)
+        {
+            // Получает координаты моба
+            coord_x = transform.position.x;
+            coord_y = transform.position.y + 0.2f;
+            // Присваивает координаты моба к тексту
+            enemy_hp.transform.position = new Vector2(coord_x, coord_y);
+            enemy_hp.text = Enemy_Health + "\u2764";
         }
     }
     IEnumerator FightCoroutine()
     {
         while (attack)
         {
-            yield return new WaitForSeconds(2f);
-            GG_health -= (int)((1 - GG_armor) * Enemy_Damage);
-            Enemy_Health -= GG_damage;
-            Debug.Log("GG: " + GG_health);
-            Debug.Log("Enemy: " + Enemy_Health);
-            if (GG_health <= 0)
+            yield return new WaitForSeconds(1f);
+            Stats.GG_Health -= (int)((1 - Stats.GG_Armor) * Enemy_Damage);
+            Enemy_Health -= Stats.GG_Damage;
+            Debug.Log("GG: " + Stats.GG_Health);
+            if (Stats.GG_Health <= 0)
             {
                 Destroy(gameObject);
                 attack = false;
@@ -55,6 +68,7 @@ public class Fights : MonoBehaviour
             {
                 Destroy(gameObject);
                 attack = false;
+                enemy_hp.text = null;
             }
         }
     }
