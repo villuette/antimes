590b73dc54939989b1e0b4a09c894b52eb4d278a
Denis <muharamov14@gmail.com>
2022-04-04 22:26:50 +0500

    перед rebase

--- Assets/Scripts/Fights.cs
+++ Assets/Scripts/Fights.cs
@@ -7,22 +7,25 @@ public class Fights : MonoBehaviour
     private bool attack = false;
     private int Enemy_Health;
     private int Enemy_Damage;
+    private bool switch_anim = true;
     public float coord_x, coord_y;
     [SerializeField] private Text enemy_hp;
+    Animator anim;
 
     void Start()
     {
         Stats_Enemy enemy = gameObject.AddComponent<Stats_Enemy>();
         Enemy_Health = enemy.Enemy_Health;
         Enemy_Damage = enemy.Enemy_Damage;
+        anim = GameObject.Find("GG").GetComponent<Animator>();
     }
-    private void Update()
+    private void FixedUpdate()
     {
         ShowEnemyHp();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.CompareTag("Player"))
         {
             attack = true;
             StartCoroutine(FightCoroutine());
@@ -30,10 +33,12 @@ public class Fights : MonoBehaviour
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.CompareTag("Player"))
         {
             attack = false;
             enemy_hp.text = null;
+            anim.SetInteger("is_fights_1", 0);
+            anim.SetInteger("is_fights_2", 0);
             StopCoroutine(FightCoroutine());
         }
     }
@@ -55,13 +60,29 @@ public class Fights : MonoBehaviour
     {
         while (attack)
         {
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(0.5f);
+
+            if (switch_anim)
+            {
+                anim.SetInteger("is_fights_1", 1);
+                anim.SetInteger("is_fights_2", 0);
+               
+                switch_anim = false;
+            }
+            else
+            {
+                anim.SetInteger("is_fights_2", 1);
+                anim.SetInteger("is_fights_1", 0);
+                switch_anim = true;
+            }
+           
+
             Stats.GG_Health -= (int)((1 - Stats.GG_Armor) * Enemy_Damage);
             Enemy_Health -= Stats.GG_Damage;
             Debug.Log("GG: " + Stats.GG_Health);
             if (Stats.GG_Health <= 0)
             {
-                Destroy(gameObject);
+                Destroy(GameObject.Find("GG"));
                 attack = false;
             }
             if (Enemy_Health <= 0)
